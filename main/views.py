29099e8946d77b71a0cc79d7bccfdda5a39dcc8d
DenisRudakov0 <denis.rudakov.2000.14@gmail.com>
2021-03-15 23:40:32 +0300

    Add_Jinja_indexhtml

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 # Отвечает за методы которые будут вызваны при переходя польователя на какуе-то определенную страницу
 def index(request):
-    return HttpResponse('<h4>Проверка связи</h4>')
+    return render(request, 'main/index.html') # метод render позволяет отображать полноценные html странички
 
 def about(request):
     return HttpResponse('<h4>Ссылка 2</h4>')
